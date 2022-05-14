b8dda703295391f17ceebe170f1a9de5494dcdb9
msojocs <jiyecafe@gmail.com>
2022-05-14 23:10:35 +0800

    perf: 优化反混淆脚本 优化反混淆脚本，可读性更高了

--- test/js-decode.js
+++ test/js-decode.js
@@ -3,68 +3,85 @@
 const fs = require('fs')
 const path = require('path')
 
-function decode(str){
-    return str.replace(/\\x(\w{2})/g,function(_,$1){ return String.fromCharCode(parseInt($1,16)) });
+function parseSubFunc (str) {
+  return str.replace(
+    /(\w+|\S)\['([a-zA-Z_]*?)'\]/g,
+    function ($0, $1, $2) {
+      let result = $0
+      switch ($1) {
+        case '{':
+          result = `{${$2}`
+          break
+        case '}':
+          result = `}${$2}`
+          break
+        case ']':
+          result = `].${$2}`
+          break
+        case '.':
+        case ':':
+          result = `${$1}${$2}`
+          break
+        case 'async':
+        case 'get':
+          result = `${$1} ${$2}`
+          break
+        default:
+          // console.log("---", $0, $1, $2)
+          result = `${$1}.${$2}`
+          break
+      }
+      // if($2 === "toString"){
+      //     console.log("---", $0, $1, $2 , "   result:", result)
+      // }
+      return result
+    }
+  )
 }
 
-function encodeUnicode(s) {
-    return s.replace(
-        /'(\\u([\da-f]{4})){1,}'/g,
-        function ($0, $1, $2) {
-            let result = eval('"' + $0 + '"')
-            // console.log('---', $0, $1, $2)
-            return result;
-        }
-    );
+function encodeUnicode (s) {
+  return s.replace(
+    /'(\\u([\da-f]{4})){1,}'/g,
+    function ($0, $1, $2) {
+      const result = eval('"' + $0 + '"')
+      // console.log('---', $0, $1, $2)
+      return result
+    }
+  )
 }
 const sourceCode = fs.readFileSync(path.resolve(__dirname, '../app/app/main/index.orgi.js'))
 
 let resultCode = sourceCode.toString()
 // let i = 0;
 resultCode = resultCode.replace(
-    /'(([\\xa-z0-9]{2,2})+)'/g,
-    function ($0, $1, $2) {
-        // i++;
-        // 二分法查找异常点
-        // 763 ok
-        // 764 error
-        // if(i >= 763){
-        //     if(i === 763){
-        //         console.log("---", $0, $1, $2)
-        //         let result = eval('"' + $1 + '"')
-        //         console.log("---", result)
-        //     }
-        //     return $0
-        // }
+  /'(([\\xa-z0-9]{2,2})+)'/g,
+  function ($0, $1, $2) {
+    // i++;
+    // 二分法查找异常点
+    // 763 ok
+    // 764 error
+    // if(i >= 763){
+    //     if(i === 763){
+    //         console.log("---", $0, $1, $2)
+    //         let result = eval('"' + $1 + '"')
+    //         console.log("---", result)
+    //     }
+    //     return $0
+    // }
 
-        let result = eval('"' + $1 + '"')
-        if(result.includes("*")){
-            // 不做处理
-            return $0
-        }
-        if(result.includes("'"))
-            return `'${result.replace(/'/g, "\\'")}'`
-        return `'${result}'`;
+    const result = eval('"' + $1 + '"')
+    if (result.includes('*')) {
+      // 不做处理
+      return $0
     }
-);
+    if (result.includes("'")) {
+      return `'${result.replace(/'/g, "\\'")}'`
+    }
+    return `'${result}'`
+  }
+)
 resultCode = encodeUnicode(resultCode)
+fs.writeFileSync(path.resolve(__dirname, '../app/app/main/index1.js'), resultCode)
+resultCode = parseSubFunc(resultCode)
+resultCode = parseSubFunc(resultCode)
 fs.writeFileSync(path.resolve(__dirname, '../app/app/main/index.js'), resultCode)
-resultCode = resultCode.replace(
-    /[\{\}\.]?\['([a-zA-Z]*?)'\]\(?/g,
-    function ($0, $1, $2) {
-        // console.log("---", $0, $1, $2)
-        let end = '', start=''
-        if($0.endsWith('('))end = '('
-        if(!$0.startsWith('['))start = $0[0]
-
-        let result = $0
-        if($0.startsWith('.'))
-            result =  `.${$1}${end}`
-        else if($0.startsWith('{') || $0.startsWith('}'))
-            result =  `${start}${$1}${end}`
-        // console.log('--- result:', result)
-        return result
-    }
-);
-fs.writeFileSync(path.resolve(__dirname, '../app/app/main/index2.js'), resultCode)
-
