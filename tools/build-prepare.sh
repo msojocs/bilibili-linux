2dc83d5f08bfa4573d848ac2501c6ef37bc41ef6
msojocs <jiyecafe@gmail.com>
2022-05-14 17:26:20 +0800

    feat: appimage

---
+++ tools/build-prepare.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 用于github actions构建
+# 解压初步构建后的文件
+
+# 脚本执行前提，已完成支持wine的基本构建
+set -e
+success() {
+    echo -e "\033[42;37m 成功 \033[0m $1"
+}
+notice() {
+    echo -e "\033[36m $1 \033[0m "
+}
+fail() {
+    echo -e "\033[41;37m 失败 \033[0m $1"
+}
+
+root_dir=$(cd `dirname $0`/.. && pwd -P)
+tmp_dir="$root_dir/tmp"
+store_dir="$tmp_dir/build"
+mkdir -p $store_dir
+rm -rf app electron
+tar -zxf bilibili-*.src/bilibili-*.tar.gz -C .
+ 
