3638b90dd191610a1af4ce77d324b854ab840027
msojocs <jiyecafe@gmail.com>
2023-06-02 22:33:39 +0800

    fix: 弹幕ext有概率无法加载

--- extensions/area_unlimit/hook/player.js
+++ extensions/area_unlimit/hook/player.js
@@ -5,6 +5,11 @@ if(pacLink.length > 0)
 if(result === 'error')localStorage.pacLink = ""
 console.log("====HOOK===PLAYER====");
 
+const sleep = (ms) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms)
+  })
+}
 (()=>{
 
   const HTTP = {
@@ -354,10 +359,19 @@ console.log("====HOOK===PLAYER====");
     }
   })()
 
-  window.onload = ()=>{
+  window.onload = async ()=>{
     console.log("====onload====")
-    const headerLeft = document.querySelector("#app > div > div.app_player--header.flex_between.draggable.db_click_max > div.app_player--header-left")
+    let headerLeft = null
 
+    for (let i = 0; true; i++) {
+      headerLeft = document.querySelector("#app > div > div.app_player--header.flex_between.draggable.db_click_max > div.app_player--header-left")
+      if (headerLeft !== null) break
+      console.error('头部元素未找到！', i)
+      await sleep(1000)
+      if (i > 20) {
+        return
+      }
+    }
     // 创建菜单元素
     const playerExtPage = document.createElement('span')
     playerExtPage.innerHTML = `<svg width="26" height="26" style="margin-right:5px" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1305" ><path d="M1023.8 604.7c0 160.9-129 291.3-287.9 291.3H256.1C113.3 888.5 0.2 781.3 0.2 650.7c0-99.4 65.6-185 159.9-223.5 19.7-8 33.8-25.5 38.2-46.3C224.9 254.9 340.8 160 480 160c102.6 0 192.4 51.5 243.4 129 10 15.3 26.1 25.5 44.2 28.2 145.2 22 256.2 142.1 256.2 287.5z" p-id="1306" fill="#ffffff"></path></svg>弹幕Ext`
