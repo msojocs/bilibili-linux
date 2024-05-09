dafd7b9c46611713198b459869f5b709037cf13d
msojocs <jiyecafe@gmail.com>
2024-05-09 15:42:11 +0800

    fix: prepare

--- tools/build-prepare.sh
+++ tools/build-prepare.sh
@@ -30,5 +30,5 @@ tmp_dir="$root_dir/tmp"
 store_dir="$tmp_dir/build"
 mkdir -p $store_dir
 rm -rf app electron
-tar -zxf "bilibili-app.src/bilibili-asar-*.tar.gz" -C .
+tar -zxf bilibili-app.src/bilibili-asar-*.tar.gz -C .
  
