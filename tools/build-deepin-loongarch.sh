3b44f0d7b6f37482ade2e429e45e8981b1da2538
msojocs <jiyecafe@gmail.com>
2024-05-10 16:08:33 +0800

    fix: group not exists

--- tools/build-deepin-loongarch.sh
+++ tools/build-deepin-loongarch.sh
@@ -9,7 +9,8 @@ catchError() {
     if [ $exit_code -ne 0 ]; then
         fail "\033[31mcommand: $2\n  at $0:$1\n  at $STEP\033[0m"
     fi
-    exit $exit_code
+    # 让runner失败
+    exit 128
 }
 
 notice() {
@@ -21,6 +22,7 @@ fail() {
 apt update
 apt install -y devscripts build-essential debhelper
 
+groupadd -g $DOCKER_GID docker
 useradd -m -u $DOCKER_UID -g $DOCKER_GID builder && passwd -d builder
 cat /etc/passwd
 su - ubuntu -c "BUILD_ARCH=$BUILD_ARCH $root_dir/tools/build-deepin.sh $BUILD_TAG"
