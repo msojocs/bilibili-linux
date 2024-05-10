a3bf3d8dc9838969921ead8e268e78b3d34c2b17
msojocs <jiyecafe@gmail.com>
2024-05-10 16:22:24 +0800

    fix: user

--- tools/build-deepin-loongarch.sh
+++ tools/build-deepin-loongarch.sh
@@ -9,8 +9,8 @@ catchError() {
     if [ $exit_code -ne 0 ]; then
         fail "\033[31mcommand: $2\n  at $0:$1\n  at $STEP\033[0m"
     fi
-    # 让runner失败
-    exit 128
+    echo $exit_code > $root_dir/tmp/exit_code
+    exit $exit_code
 }
 
 notice() {
@@ -25,4 +25,4 @@ apt install -y devscripts build-essential debhelper
 groupadd -g $DOCKER_GID docker
 useradd -m -u $DOCKER_UID -g $DOCKER_GID builder && passwd -d builder
 cat /etc/passwd
-su - ubuntu -c "BUILD_ARCH=$BUILD_ARCH $root_dir/tools/build-deepin.sh $BUILD_TAG"
+su - builder -c "BUILD_ARCH=$BUILD_ARCH $root_dir/tools/build-deepin.sh $BUILD_TAG"
