b2b03e412d5b5d6b2a598fcf60ee81f070cffdb9
msojocs <jiyecafe@gmail.com>
2024-08-10 15:57:49 +0800

    perf: loong64 build

--- tools/build-deepin-loongarch.sh
+++ tools/build-deepin-loongarch.sh
@@ -19,10 +19,34 @@ notice() {
 fail() {
     echo -e "\033[41;37m 失败 \033[0m $1"
 }
+
+DOCKER_UID=$(id -u)
+DOCKER_GID=$(id -g)
+BUILD_TAG=${BUILD_TAG:-1.0.0}
+BUILD_ARCH=${BUILD_ARCH:-loong64}
+
+# why use docker to build? a) old system does support loong64.
+docker run \
+    --rm -i \
+    -e "DOCKER_GID=${DOCKER_GID}" \
+    -e "DOCKER_UID=${DOCKER_UID}" \
+    -e "BUILD_TAG=${BUILD_TAG}" \
+    -e "BUILD_ARCH=${BUILD_ARCH}" \
+    -w /workspace \
+    -v "$root_dir:/workspace" \
+    ubuntu:24.04 \
+    sh <<\EOF
 apt update
 apt install -y devscripts build-essential debhelper
 
+echo $DOCKER_GID
+echo $DOCKER_UID
+echo $BUILD_TAG
+echo $BUILD_ARCH
 groupadd -g $DOCKER_GID docker
 useradd -m -u $DOCKER_UID -g $DOCKER_GID builder && passwd -d builder
 cat /etc/passwd
-su - builder -c "BUILD_ARCH=$BUILD_ARCH $root_dir/tools/build-deepin.sh $BUILD_TAG"
+user=$(getent passwd $DOCKER_UID | cut -d: -f1)
+su - $user -c "BUILD_ARCH=$BUILD_ARCH /workspace/tools/build-deepin.sh $BUILD_TAG"
+EOF
+
