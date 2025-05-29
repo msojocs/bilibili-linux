60094496b8ff2def2c8d74cc0b6e0afb88bce62e
msojocs <jiyecafe@gmail.com>
2025-05-29 22:57:49 +0800

    update: bilibili to v1.16.5.4265

--- tools/fix-other.sh
+++ tools/fix-other.sh
@@ -41,8 +41,8 @@ notice "屏蔽检测"
 # grep -lr 'if (!dj' --exclude="app.asar" .
 # sed -i 's#if (!dj#if(false\&\&!dj#g' "app/main/app.js"
 # ==='win';if(! 警告11
-grep -lr 'if (!j4)' --exclude="app.asar" .
-sed -i 's#if (!j4)#if(false\&\&!j4)#' "app/main/app.js"
+grep -lr 'if (!j5)' --exclude="app.asar" .
+sed -i 's#if (!j5)#if(false\&\&!j5)#' "app/main/app.js"
 # global['bootstrapApp']();
 # grep -lr 'if (dj)' --exclude="app.asar" .
 # sed -i 's#if (dj)#if(!dj)#' "app/main/app.js"
