fb2a73bd00a222d88bbe4dafe8ba043fd2be69d8
msojocs <jiyecafe@gmail.com>
2025-08-10 12:40:17 +0800

    feat: cursor support for wayland

--- tools/cursor-tool/main.go
+++ tools/cursor-tool/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"cursor-tool/libinput"
+	"fmt"
 	"log"
 	"time"
 
@@ -17,17 +18,17 @@ func main() {
 	li := libinput.NewPathContext()
 	defer li.Close()
 
-	log.Println("start add device")
+	// log.Println("start add device")
 	if err := li.PathAddDevice(testDevice); err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("start poll")
+	// log.Println("start poll")
 	fd := li.GetPollFD()
 
 	for {
-		log.Println("please generate input")
-		n, err := unix.Poll([]unix.PollFd{fd}, 10000)
+		// log.Println("please generate input")
+		n, err := unix.Poll([]unix.PollFd{fd}, 1000)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -38,21 +39,21 @@ func main() {
 		if n <= 0 {
 			log.Fatal("no events received")
 		}
-		log.Println("ecent count:", n)
+		// log.Println("ecent count:", n)
 
 		ev, err := li.GetEvent()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		log.Printf("event: %+v\n", ev)
-		log.Printf("type: %s\n", ev.Type)
+		// log.Printf("event: %+v\n", ev)
+		// log.Printf("type: %s\n", ev.Type)
 		if ev.Type == libinput.Event_PointerMotionAbsolute {
 			p, err := ev.GetAbsolutePosition()
 			if err != nil {
 				log.Fatalln("absolute position error:", err)
 			}
-			log.Println("x:", p.X, ", y:", p.Y)
+			fmt.Printf("%d,%d\n", uint32(p.X), uint32(p.Y))
 			break
 		}
 		time.Sleep(100 * time.Millisecond)
