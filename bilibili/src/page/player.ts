3f57ada477eebf606839aff2cb03649a0136d64c
msojocs <jiyecafe@gmail.com>
2025-08-26 22:49:13 +0800

    fix: 倍速

--- bilibili/src/page/player.ts
+++ bilibili/src/page/player.ts
@@ -19,12 +19,12 @@ export const initPlayerPage = () => {
 
     for (let i = 0; ; i++) {
       headerLeft = document.querySelector("#app > div > div.app_player--header.flex_between.draggable.db_click_max > div.app_player--header-left")
-      if (headerLeft !== null) break
-      log.info('头部元素未找到！', i)
-      await sleep(1000)
-      if (i > 20) {
-        return
+      if (headerLeft === null) {
+        log.error('头部元素未找到！', i)
+        await sleep(1000)
+        continue
       }
+      break
     }
 
     const root = document.createElement('div')
@@ -35,58 +35,67 @@ export const initPlayerPage = () => {
       headerLeft.append(root)
     createRoot(root).render(SettingButton())
 
-    {
-      const createElement = (apeedRate: number) => {
-        const rate = document.createElement('li')
-        rate.className = "bpx-player-ctrl-playbackrate-menu-item"
-        rate.dataset.value = `${apeedRate}`
-        rate.textContent = `${apeedRate}x`
-        return rate
+    for (let i=0;;i++){
+      const danmakuManage = window.danmakuManage
+      if (!danmakuManage){
+        await sleep(1000)
+        continue
       }
-      const speedRate = window.danmakuManage.nodes.controlBottomRight.querySelector('.bpx-player-ctrl-playbackrate-menu > li:nth-child(1)')
-      
-      speedRate!.after(createElement(1.75))
-      speedRate!.before(createElement(4.0))
-      speedRate!.before(createElement(3.5))
-      speedRate!.before(createElement(3.0))
-      speedRate!.before(createElement(2.5))
-    }
-    {
-      let originalFilter = window.danmakuManage.danmaku.config.fn.filter
-      const customFilter = (t: {colorful: boolean, colorfulImg: string, weight: number}) => {
-        log.info('filter....')
-        if (originalFilter(t)){
-          // log.info('default block:', t.weight)
-          return true
+      log.info('找到弹幕管理器', danmakuManage)
+      {
+        const createElement = (apeedRate: number) => {
+          const rate = document.createElement('li')
+          rate.className = "bpx-player-ctrl-playbackrate-menu-item"
+          rate.dataset.value = `${apeedRate}`
+          rate.textContent = `${apeedRate}x`
+          return rate
         }
-        if (localStorage.getItem('dm-filter-blockvip') === 'true')
-        {
-          // 屏蔽大会员彩色
-          if (t.colorful || t.colorfulImg) {
-            log.info('block vip', JSON.stringify(t, null, 4))
+        const speedRate = danmakuManage.nodes.controlBottomRight.querySelector('.bpx-player-ctrl-playbackrate-menu > li:nth-child(1)')
+        
+        speedRate!.after(createElement(1.75))
+        speedRate!.before(createElement(4.0))
+        speedRate!.before(createElement(3.5))
+        speedRate!.before(createElement(3.0))
+        speedRate!.before(createElement(2.5))
+      }
+      {
+        let originalFilter = danmakuManage.danmaku.config.fn.filter
+        const customFilter = (t: {colorful: boolean, colorfulImg: string, weight: number}) => {
+          log.info('filter....')
+          if (originalFilter(t)){
+            // log.info('default block:', t.weight)
             return true
           }
-          
+          if (localStorage.getItem('dm-filter-blockvip') === 'true')
+          {
+            // 屏蔽大会员彩色
+            if (t.colorful || t.colorfulImg) {
+              log.info('block vip', JSON.stringify(t, null, 4))
+              return true
+            }
+            
+          }
+          const weight = parseInt(localStorage.getItem('dm-filter-weight') || '0')
+          if (t.weight <= weight) {
+            log.info('current weight:', weight)
+            log.info('block weight:', JSON.stringify(t, null, 4))
+            return true
+          }
+          return false
         }
-        const weight = parseInt(localStorage.getItem('dm-filter-weight') || '0')
-        if (t.weight <= weight) {
-          log.info('current weight:', weight)
-          log.info('block weight:', JSON.stringify(t, null, 4))
-          return true
+        danmakuManage.danmaku.config.fn.filter = customFilter
+        {
+          const originalInitDanmaku = danmakuManage.initDanmaku
+          danmakuManage.initDanmaku = function () {
+            log.info('initDanmaku...')
+            originalInitDanmaku.apply(this)
+            log.info('update filter...')
+            originalFilter = this.danmaku.config.fn.filter
+            danmakuManage.danmaku.config.fn.filter = customFilter
+          }
         }
-        return false
       }
-      window.danmakuManage.danmaku.config.fn.filter = customFilter
-      {
-        const originalInitDanmaku = window.danmakuManage.initDanmaku
-        window.danmakuManage.initDanmaku = function () {
-          log.info('initDanmaku...')
-          originalInitDanmaku.apply(this)
-          log.info('update filter...')
-          originalFilter = this.danmaku.config.fn.filter
-          window.danmakuManage.danmaku.config.fn.filter = customFilter
-        }
-      }
+      break
     }
   })
 }
