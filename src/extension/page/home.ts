77fcf8fe030995dff966129dc06f48d58923c8a4
msojocs <jiyecafe@gmail.com>
2026-05-09 19:44:58 +0800

    perf: 精选页面不显示设置按钮

--- src/extension/page/home.ts
+++ src/extension/page/home.ts
@@ -4,6 +4,23 @@ import { createLogger, Logger } from "../../common/log"
 import { replaceXMLHttpRequest } from "../document/replace"
 import { sleep } from "../../common/utils"
 
+type HistoryStateMethod = 'pushState' | 'replaceState'
+type HistoryStateArgs = Parameters<History['pushState']>
+{
+  const wrapHistory = (type: HistoryStateMethod) => {
+    const orig = history[type]
+
+    return function(this: History, ...args: HistoryStateArgs) {
+      const rv = orig.apply(this, args)
+      window.dispatchEvent(new CustomEvent<HistoryStateArgs>(type, { detail: args }))
+      return rv
+    }
+  }
+
+  history.pushState = wrapHistory('pushState')
+  history.replaceState = wrapHistory('replaceState')
+}
+
 export const initHomePage = () => {
   Logger.moduleName = 'Home'
   const log = createLogger('Entry')
@@ -21,7 +38,20 @@ export const initHomePage = () => {
       root.classList.add('custom-setting')
       buttonArea.prepend(root)
       createRoot(root).render(SettingEntry())
+      if (window.location.href.endsWith('/selected')) {
+        root.style.display = 'none'
+      }
+      window.addEventListener('replaceState', (e: Event) => {
+        const args = (e as CustomEvent<HistoryStateArgs>).detail
+        const target = args?.[0]?.forward
+        if (typeof target === 'string' && target?.endsWith('/selected')) {
+          root.style.display = 'none'
+        } else {
+          root.style.display = 'block'
+        }
+      })
       break
     }
+
   })
 }
